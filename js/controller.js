883037af600b41f1e08276a98c9cc42c24e4dd1e
Maxence LAURENT <maxence.laurent@production.infra>
2015-12-15 15:47:26 +0100

    angular2

--- js/controller.js
+++ js/controller.js
@@ -1,34 +1,34 @@
-'use strict';
 
-modis.controller('add', function () {
 
+modis.controller('TyresController', [function(TyresService) {
+	var initEve =['click'];
 	on('event[action="add"]', initEve, function(){
 			document.getElementById('form').style.display = 'block';
 	});	
-});
+}]);
 
-modis.controller('delete',function(){
+// modis.controller('delete',function(){
 
-	off('event[action="delete"]', initEve, function(){
-			//modis.service.delete();
-	});
-});
+// 	off('event[action="delete"]', initEve, function(){
+// 			//modis.service.delete();
+// 	});
+// });
 
-modis.controller('edit', function () {
+// modis.controller('edit', function () {
 
-	on('event[action="edit"]', initEve, function(){
-		////modis.service.post();
-	});		
-});
+// 	on('event[action="edit"]', initEve, function(){
+// 		////modis.service.post();
+// 	});		
+// });
 
-modis.controller('save', function () {
+// modis.controller('save', function () {
 
-	on('event[action="save"]', initEve, function(){
-		$('#form')[0].style.display = 'none';
+// 	on('event[action="save"]', initEve, function(){
+// 		$('#form')[0].style.display = 'none';
 		
-		var newTyre = tyreForm('.form',elements);
-		modis.view.addTyre('list', newTyre);
+// 		var newTyre = tyreForm('.form',elements);
+// 		modis.view.addTyre('list', newTyre);
 	
-		modis.inject('service', 'post')(newTyre);
-	});	
-});
+// 		modis.inject('service', 'post')(newTyre);
+// 	});	
+// });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-var elements = ['reference', 'vehiculeType', 'pattern_height','pattern_width','pattern_thickness','other'];
-var initEve = ['click'];
+// var elements = ['reference', 'vehiculeType', 'pattern_height','pattern_width','pattern_thickness','other'];
+// var initEve = ['click'];
 
-(function(){
-	modis.inject('controller','add')();
-	modis.inject('controller','save')();	
-})();
+// (function(){
+// 	modis.inject('controller','add')();
+// 	modis.inject('controller','save')();	
+// })();
+
+var imports = ['ngRoute'];
+
+var modis = angular.module('modis', imports);
+
+modis.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'partials/tyres.tpl',
+        controller: 'TyresController'
+	});
+}]);
